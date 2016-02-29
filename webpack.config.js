ff780b57e403789c85d958fa9e462643a5a4f841
christianrest <christian.rest@codecentric.de>
2016-02-29 18:35:18 +0100

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+
+module.exports = {
+	entry: [
+		'./src/index.js'
+	],
+	module: {
+	    loaders: [{
+	      test: /\.js$/,
+	      exclude: /node_modules/,
+	      loader: 'babel'
+	    }]
+	},
+	resolve: {
+	    extensions: ['', '.js', '.jsx']
+	},
+	output: {
+		path: __dirname + '/dist',
+		publicPath: '/',
+		filename: 'client.js'
+	},
+	devServer: {
+		contentBase: './dist',
+	}
+}
